f116f9a68058f0484573aa828b50bf21f6c132e9
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-08-25 14:46:57 +0200

    added color to type in projects card

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -15,7 +15,7 @@
                 @endif
                 <div class="card-body">
                     <h5 class="card-title">{{ $project->title }}</h5>
-                    <h6 class="card-title">{{ $project->type->name }}</h6>
+                    <h6 class="card-title ivy_card_type" style="color: {{ $project->type->color }};">{{ $project->type->name }}</h6>
                     <h6 class="card-text">{{ $project->topic }}</h6>
                     <p class="card-text text-secondary">{{ $project->date }}</p>
                     <p class="card-text">{{ $project->gitHub }}</p>
